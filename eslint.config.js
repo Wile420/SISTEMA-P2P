223493248e4b3c58acd64469dec444e956daab26
Wile420 <av5000216@gmail.com>
2026-05-30 16:12:02 -0400

    chore: configurar github actions, eslint y prettier para integracion continua

---
+++ eslint.config.js
@@ -0,0 +1,15 @@
+module.exports = [
+  {
+    ignores: ["node_modules/", "dist/"]
+  },
+  {
+    languageOptions: {
+      ecmaVersion: "latest",
+      sourceType: "module",
+    },
+    rules: {
+      "no-unused-vars": "warn",
+      "no-console": "off"
+    }
+  }
+];
